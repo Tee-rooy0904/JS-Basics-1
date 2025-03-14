6a36b726a616aa0bb3b1f0b79ec97ff77ee1a7d8
Tee-rooy0904 <troyr1203@gmail.com>
2025-03-14 09:50:46 +0800

    JSBasics
    jsbasics

--- js3/main.js
+++ js3/main.js
@@ -1,33 +1,50 @@
 const elementTxt = document.getElementById("elements");
-let numbInput = document.getElementById("numbInput")
+let numInpt = document.getElementById('numInpt');
 const sumTxt = document.getElementById("sum");
-const highTxt = document.getElementById("high")
-const lowTxt = document.getElementById("low")
+const highNum = document.getElementById("highNum");
+const lowNum = document.getElementById("lowNum");
 let sum = 0;
 let largest=0;
 
 let arr = [];
 
-function insertNum() {
-    arr.push(parseInt(numbInput.value));
-    let smallest=arr[0];
-    for (let i = 0; i <arr.length; i++) {
+
+
+
+function insertEl() {
+    arr.push(parseInt(numInpt.value));
+    let smallests= arr[0];
+    for (let i = 0; i < arr.length; i++) {
         sum += arr[i];
     }
 
     for (let i = 0; i < arr.length; i++) {
-        i(arr[i] < smallest){
-            smallest = arr[i];
+        if(arr[i] < smallests){
+            smallests = arr[i];
         }
-        if (arr[i] > largest){
+        if(arr[i] > largest){
             largest = arr[i];
         }
+        
+        
+    }
+
+    elementTxt.innerHTML = arr.join("<br>");
+    sumTxt.innerHTML = sum;
+    highNum.innerHTML = largest;
+    lowNum.innerHTML = smallests;
+    console.log(arr);
 
 }
 
-elementTxt.innerHTML = arr.join("<br");
-sumTxt.innerHTML = sum;
-highTxt.innerHTML = largest;
-lowTxt.innerHTML = smallest;
-console.log(arr);
+
+function deleteAll(){
+    arr=[]
+    elementTxt.innerHTML = arr.join("<br>");
+    sumTxt.innerHTML = 0;
+    highNum.innerHTML = 0;
+    lowNum.innerHTML = 0;
+    console.log(arr);
 }
+
+
